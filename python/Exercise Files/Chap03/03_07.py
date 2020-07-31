4e9d83cef069c92e8fa4dfb992d15bb9dbc91f67
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-07-31 05:27:29 +0200

    Challenge 1

--- python/Exercise Files/Chap03/03_07.py
+++ python/Exercise Files/Chap03/03_07.py
@@ -1,13 +1,16 @@
 print("Challenge 1:")
 
 # A message for the user
-message = "This is going to be tricky ;)"
-Message = "Very tricky!"
-print(message) # show the message on the screen
+message = "This is going to be tricky ;)"  # m small
+Message = "Very tricky!"  # m capital
+print(message)  # show the message on the screen
+
+# output ==> This is going to be tricky ;)
+
 
 # Perform mathematical operations
 result = 2**3
-print("2**3 =", result)
+print("2**3 =", result)  # output => 8
 result = 5 - 3
 #print("5 - 3 =", result)
 
